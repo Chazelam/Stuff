497b8fc99cae2d7bfb71cdb1c37d564a3cdea22e
Chazelam <andreilaymzin@gmail.com>
2023-03-17 12:23:04 +0300

    Done George's_shit.py

---
+++ George's_shit.py
@@ -0,0 +1,27 @@
+def sum_n(k, n):
+    Sum = 0
+    while k < n:
+        Sum = Sum + s(k)
+        k += 1
+    return Sum
+
+def s(k):
+    return (2**k)*(a(k)**2 - b(k)**2)
+
+def a(k):
+    if k == 0:
+        return 1
+    else: 
+        return (a(k-1)+b(k-1))/2
+
+def b(k):
+    if k == 0:
+        return 1/(2**0.5)
+    else:
+        return (a(k-1)*b(k-1))**0.5
+
+def U(x):
+    return (2*(a(x+1)**2))/(1 - sum_n(0, x))
+
+n = int(input("n: ")) + 1
+print("Pi: ", U(n))
